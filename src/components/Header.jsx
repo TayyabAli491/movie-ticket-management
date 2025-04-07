97f641945cac7fdaade98311e3f8a82b88acd809
TayyabAli491 <alihaniff5@gmail.com>
2025-04-07 06:06:40 +0500

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,24 +39,6 @@ export default function Header() {
             >
               Home
             </Link>
-            <Link 
-              to="/movies" 
-              className="px-4 py-2 text-md font-medium text-gray-300 hover:text-red-400 transition-colors relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-px after:bg-blue-600 hover:after:w-full after:transition-all"
-            >
-              Movies
-            </Link>
-            <Link 
-              to="/about" 
-              className="px-4 py-2 text-md font-medium text-gray-300 hover:text-red-400 transition-colors relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-px after:bg-blue-600 hover:after:w-full after:transition-all"
-            >
-              About
-            </Link>
-            <Link 
-              to="/contact" 
-              className="px-4 py-2 text-md font-medium text-gray-300 hover:text-red-400 transition-colors relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-px after:bg-blue-600 hover:after:w-full after:transition-all"
-            >
-              Contact
-            </Link>
           </div>
 
           {/* Auth Buttons - Desktop */}
